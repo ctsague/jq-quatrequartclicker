a2484e1260d23d4b6661903bc5f0b049b2053718
tsague <tsagueymele@yahoo.fr>
2016-09-30 15:02:22 +0200

    exo sans bonus

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
+
+
+	//initialiser la var coubt par la valeur de span
+	var count = 0;
+	//var tab=["oups","Serieux","va faire un tour!"];
+	//var len=tab.lenght;
 	//votre code
+	//quand je clique sur le cake j'incremente mon count
+	$("img.cake-is-a-lie").click(function(){
+		count++;
+		$(".counter").text(count);
+			
+		
+	});
+
+
+
+
+	
 
 
 });
