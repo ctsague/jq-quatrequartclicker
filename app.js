7622743e1056098997ae26aed3f86d5681ab2227
tsague <tsagueymele@yahoo.fr>
2016-09-30 15:10:39 +0200

    with bonus 2

--- app.js
+++ app.js
@@ -3,23 +3,18 @@ $(document).ready(function(){
 
 	//initialiser la var coubt par la valeur de span
 	var count = 0;
-	//var tab=["oups","Serieux","va faire un tour!"];
-	//var len=tab.lenght;
+	tab=["oups","Serieux","va faire un tour!"];
+	len=tab.length;
 	//votre code
 	//quand je clique sur le cake j'incremente mon count
 	$("img.cake-is-a-lie").click(function(){
 		count++;
 		$(".counter").text(count);
 		if(count % 20=== 0){
-			alert("va voir ailleurs");
-			}
-
-			
+			alert(tab[Math.floor(Math.random()*len)]);
+			}		
 		
 	});
-
-
-
 
 	
 
