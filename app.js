9f95cfe13b516e4639d4b6a0dc8e9eee25033867
Amine EL G <aelghayate@simplon.co>
2016-10-07 13:45:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 $(document).ready(function(){
-
-
 	//initialiser la var coubt par la valeur de span
 	var count = 0;
-	tab=["oups","Serieux","va faire un tour!"];
+	var tab=["oups","Serieux","va faire un tour!"];
 	len=tab.length;
-	//votre code
 	//quand je clique sur le cake j'incremente mon count
 	$("img.cake-is-a-lie").click(function(){
 		count++;
 		$(".counter").text(count);
-		if(count % 20=== 0){
-			alert(tab[Math.floor(Math.random()*len)]);
-			}		
-		
+		if(count % 20 === 0){
+			var message = tab[Math.floor(Math.random()*len)];
+			alert(message);
+		}		
 	});
-
-	
-
-
 });
