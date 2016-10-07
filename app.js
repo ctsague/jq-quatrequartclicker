14e7535b9d4e70c4fb269ec6508eb3b10bc7d88b
ctsague <tsagueymele@yahoo.fr>
2016-10-07 13:59:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 $(document).ready(function(){
 
-
 	//initialiser la var coubt par la valeur de span
 	var count = 0;
-	tab=["oups","Serieux","va faire un tour!"];
-	len=tab.length;
-	//votre code
+	var tab=["oups","Serieux","va faire un tour!"];
+	var len=tab.length;
 	//quand je clique sur le cake j'incremente mon count
 	$("img.cake-is-a-lie").click(function(){
 		count++;
 		$(".counter").text(count);
 		if(count % 20=== 0){
-			alert(tab[Math.floor(Math.random()*len)]);
-			}		
-		
+			var message = tab[Math.floor(Math.random()*len)];
+			alert(message);
+		}			
 	});
-
-	
-
-
 });
